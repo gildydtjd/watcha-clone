ab8df27ed38e8c8061921e19a68f6094db6bd354
길용성 <gildydtjd@naver.com>
2021-09-20 17:43:53 +0900

    Feat: login-background-image

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -3,9 +3,10 @@ import Header from '../components/header/Header';
 import LoginForm from '../components/login/LoginForm';
 const LoginDiv = styled.div`
   height: 100vh;
-  background-image: url('/assets/img/login.jpg');
+  background-image: url('/assets/img/harry.jpg');
   background-size: cover;
   background-position: center;
+  background-position-y: top;
 `;
 function Login() {
   return (
